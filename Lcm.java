eba7795dbb0ea04e955b4ce4dd2ba596c7ab1e41
Codesan <tawalesanket7@gmail.com>
2023-01-08 16:56:21 +0530

    Program Commit

---
+++ Lcm.java
@@ -0,0 +1,32 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class Lcm {
+
+    private static int findLcm(int a, int b) {
+        return (a * b)/optimizedEculidGcd(a,b);
+    }
+
+    private static int optimizedEculidGcd(int a, int b) {
+        while (a != 0 && b !=0){
+            if(a>b){
+                a = a % b;
+            }else {
+                b = b % a;
+            }
+        }
+        return Math.max(a,b);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter the number 1: ");
+        int a= scanner.nextInt();
+        System.out.print("Enter the number 2: ");
+        int b= scanner.nextInt();
+
+        System.out.println("Lcm is "+findLcm(a,b));
+    }
+}
+
