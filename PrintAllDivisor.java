1acd63c9a830394e6aebfbf0531b0c04cd2245ce
Codesan <tawalesanket7@gmail.com>
2023-01-09 18:47:27 +0530

    Program commit

--- PrintAllDivisor.java
+++ PrintAllDivisor.java
@@ -4,10 +4,26 @@ import java.util.Scanner;
 
 public class PrintAllDivisor {
 
-    static void printDivisor(int n) {
+/*
+  static void printDivisor(int n) {
         for (int i = 1; i <= n; i++) {
             if (n % i == 0){
                 System.out.print(i + " ");
+            }
+        }
+    }
+    */
+
+    static  void printAllDivisors(int n){
+        int i=0;
+        for (  i= 1; i * i < n; i++) {
+            if(n %i==0){
+                System.out.print(i + " ");
+            }
+        }
+        for (; i >= 1 ; i--) {
+            if(n %i==0 && i != n/i){
+                System.out.print(n/i + " ");
             }
         }
     }
@@ -16,6 +32,7 @@ public class PrintAllDivisor {
         Scanner scanner =new Scanner(System.in);
         System.out.print("Enter the number : ");
         int n= scanner.nextInt();
-        printDivisor(n);
+        //printDivisor(n);
+        printAllDivisors(n);
     }
 }
